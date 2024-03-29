290d001c75a8f837068c7d2d0e95f469569f8552
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2024-03-29 16:30:12 +0530

    dddd

--- components/FooterBottom.js
+++ components/FooterBottom.js
@@ -4,6 +4,22 @@ import Image from "next/image";
 export default function FooterBottom11() {
   return (
     <>
+      <div class="FooterMediaIcons">
+        <div>
+          <div class="mediaFlex">
+            <a href="#zero" class="mediaFlex1">
+              Get a quote
+            </a>
+            <a
+              href="https://api.whatsapp.com/send?phone=971559564135&amp;text=I%20would%20like%20to%20know%20more%20about%20McCollins%20Media"
+              class="mediaFlex2"
+              target="_blank"
+            >
+              WhatsApp us
+            </a>
+          </div>
+        </div>
+      </div>
       <div className="FooterBottom">
         <div className="container">
           <div className="FtBtmLinkRow">
@@ -21,54 +37,56 @@ export default function FooterBottom11() {
                 </div>
               </div>
               <div className="FtBtmLinkColumn">
-                <a href="#">About Us</a>
+                <a href="#three">What We Do</a>
               </div>
               <div className="FtBtmLinkColumn">
-                <a href="#">Services</a>
+                <a href="#one">Our Brands</a>
               </div>
               <div className="FtBtmLinkColumn">
-                <a href="#">Case Studies</a>
+                <a href="#two">Case Studies</a>
               </div>
               <div className="FtBtmLinkColumn">
-                <a href="#">Strategic Partnerships</a>
+                <a href="#five">  Our Partners</a>
               </div>
               <div className="FtBtmLinkColumn">
-                <a href="#">Industries</a>
+                <a href="#six"> Our Services</a>
               </div>
               <div className="FtBtmLinkColumn">
-                <a href="#"> Contact Us</a>
+                <a href="#zero"> Contact Us</a>
               </div>
             </div>
           </div>
           <div className="FtBtm">
             <div className="row">
-                <div className="col-md-4">
-                    <div className="FtBtm1">© 2024 Performance Shark. All Right Reserved  </div>
+              <div className="col-md-4">
+                <div className="FtBtm1">
+                  © 2024 Performance Shark. All Right Reserved{" "}
                 </div>
-                <div className="col-md-4">
-                    <div className="FtBtm2">
-                        <a href="#">Terms & Conditions </a>
-                        <a href="#">Privacy Policy</a>
-                    </div>
+              </div>
+              <div className="col-md-4">
+                <div className="FtBtm2">
+                  <a href="#">Terms & Conditions </a>
+                  <a href="#">Privacy Policy</a>
                 </div>
-                <div className="col-md-4">
-                    <div className="FtBtm3">
-                        <div className="SocialMediaIcons">
-                            <a target="_blank" href="#">
-                                <div className="socialicon ticktock">&nbsp;</div>
-                            </a>
-                            <a target="_blank" href="#">
-                                <div className="socialicon facebook">&nbsp;</div>
-                            </a>
-                            <a target="_blank" href="#">
-                                <div className="socialicon instagram">&nbsp;</div>
-                            </a>
-                            <a target="_blank" href="#">
-                                <div className="socialicon linkedin m-0">&nbsp;</div>
-                            </a>
-                        </div>
-                    </div>
+              </div>
+              <div className="col-md-4">
+                <div className="FtBtm3">
+                  <div className="SocialMediaIcons">
+                    <a target="_blank" href="#">
+                      <div className="socialicon ticktock">&nbsp;</div>
+                    </a>
+                    <a target="_blank" href="#">
+                      <div className="socialicon facebook">&nbsp;</div>
+                    </a>
+                    <a target="_blank" href="#">
+                      <div className="socialicon instagram">&nbsp;</div>
+                    </a>
+                    <a target="_blank" href="#">
+                      <div className="socialicon linkedin m-0">&nbsp;</div>
+                    </a>
+                  </div>
                 </div>
+              </div>
             </div>
           </div>
         </div>
