174eb9b1212a708a7ef4e66680f0127105c81f35
de-salman <salmanfarisi.bca@gmail.com>
2024-04-25 12:35:41 +0400

    email config

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -1,60 +1,115 @@
 import Head from "next/head";
 import Image from "next/image";
 import useMediaQuery from "@/hooks/useMediaQuery";
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 // importing aos
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function HeroBanner() {
   const isDesktop = useMediaQuery("(min-width: 960px)");
   useEffect(() => {
     AOS.init();
   }, []);
+
+  // Inside your HeroBanner component or a separate form handling component
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const name = event.target.elements[0].value;
+    const email = event.target.elements[1].value;
+    const contact = event.target.elements[2].value;
+    const message = event.target.elements[3].value;
+
+    try {
+      console.log(name, email, contact, message);
+      const response = await fetch("/api/sendEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, contact, message }),
+      });
+
+      if (response.ok) {
+        alert("Email sent successfully");
+      } else {
+        alert("Error sending email");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Error sending email");
+    }
+  };
+
   return (
     <>
-     <div className="HeroBannerWrp">
-     
-      <div className="DeskHero">
-      {isDesktop && (
-        <div className="BannerImageBox">
-        <Image
-           src="/herobanner.jpg"
-           layout="responsive"
-           width={"1492"}
-           height={"816"}
-           priority={true}
-            className="MainBanner"
-         />
-        </div>
-        )}
-        <div className="BannerContentBox">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8">
-                <div className="Bt1"  data-aos="fade-up" data-aos-duration="700">Performance  Marketing
-that Drives Revenue</div>
-<div className="Bt2"  data-aos="fade-up" data-aos-duration="700">Amplify your business with our data-centric, performance-driven digital marketing solutions</div>
-              </div>
-              <div className="col-md-4">
-                <div className="BannerForm"  data-aos="fade-up" data-aos-duration="700">
-                  {/* <div className="Bt3">Build trust with your audience by writing content that
+      <div className="HeroBannerWrp">
+        <div className="DeskHero">
+          {isDesktop && (
+            <div className="BannerImageBox">
+              <Image
+                src="/herobanner.jpg"
+                layout="responsive"
+                width={"1492"}
+                height={"816"}
+                priority={true}
+                className="MainBanner"
+              />
+            </div>
+          )}
+          <div className="BannerContentBox">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-8">
+                  <div
+                    className="Bt1"
+                    data-aos="fade-up"
+                    data-aos-duration="700"
+                  >
+                    Performance Marketing that Drives Revenue
+                  </div>
+                  <div
+                    className="Bt2"
+                    data-aos="fade-up"
+                    data-aos-duration="700"
+                  >
+                    Amplify your business with our data-centric,
+                    performance-driven digital marketing solutions
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div
+                    className="BannerForm"
+                    data-aos="fade-up"
+                    data-aos-duration="700"
+                  >
+                    {/* <div className="Bt3">Build trust with your audience by writing content that
 actually answers their questions and solves their problems.</div> */}
-                  <div className="BannerFormItem">
-                    <input type="text" placeholder="Your Name*"/>
-                    <input type="text" placeholder="Email Address*"/>
-                    <input type="text" placeholder="Contact Number*"/>
-                    <input type="text" placeholder="Website URL"/>
-                    <button>Submit Inquiry</button>
+                    <form onSubmit={handleSubmit}>
+                      <div className="BannerFormItem cninput">
+                        <input type="text" placeholder="Your Name*" required />
+                        <input
+                          type="email"
+                          placeholder="Email Address*"
+                          required
+                        />
+                        <input
+                          type="number"
+                          placeholder="Contact Number*"
+                          required
+                        />
+                        <input type="text" placeholder="Website URL" required />
+                        <button type="submit">Submit Inquiry</button>
+                      </div>
+                    </form>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div> 
-     </div>
+      </div>
     </>
   );
 }
---
+++ pages/api/sendEmail.js
@@ -0,0 +1,48 @@
+// pages/api/sendEmail.js
+import nodemailer from "nodemailer";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { name, email, message } = req.body;
+
+    // Configure Nodemailer transport
+    const transporter = nodemailer.createTransport({
+      host: "smtpout.secureserver.net",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "accounts@performancesharks.com",
+        pass: "G$i]A4JYCjjqSG3",
+      },
+    });
+
+    const leadEmailOptions = {
+      from: "accounts@performancesharek.com",
+      to: "salman@mccollinsmedia.com",
+      subject: "New Lead from Website",
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    };
+
+    // Email options for the thank you message
+    const thankYouEmailOptions = {
+      from: "accounts@performancesharek.com",
+      to: email,
+      subject: "Thank You for Your Inquiry",
+      text: "Thank you for your inquiry. We will get back to you shortly.",
+    };
+
+    try {
+      // Send the lead email
+      await transporter.sendMail(leadEmailOptions);
+      // Send the thank you email
+      await transporter.sendMail(thankYouEmailOptions);
+
+      res.status(200).json({ status: "Email sent successfully" });
+    } catch (error) {
+      console.error("Error sending email:", error);
+      res.status(500).json({ status: "Error sending email" });
+    }
+  } else {
+    res.status(405).json({ status: "Method not allowed" });
+  }
+}
