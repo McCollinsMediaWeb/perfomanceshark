c73154e326dfb21258e6abfca82f5c7fa22b2a4a
de-salman <salmanfarisi.bca@gmail.com>
2024-04-25 20:13:08 +0400

    email config 5

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -17,13 +17,13 @@ export default async function handler(req, res) {
       },
       requireTLS: true,
       auth: {
-        user: "hello@performancesharks.com",
-        pass: "G$i]A4JYCjjqSG3",
+        user: "info@performancesharks.com",
+        pass: "G$i]A4JYCjjqSG4",
       },
     });
 
     const leadEmailOptions = {
-      from: "hello@performancesharek.com",
+      from: "info@performancesharks.com",
       to: "salman@mccollinsmedia.com",
       subject: "New Lead from Website",
       text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
 
     // Email options for the thank you message
     const thankYouEmailOptions = {
-      from: "hello@performancesharek.com",
+      from: "info@performancesharks.com",
       to: email,
       subject: "Thank You for Your Inquiry",
       text: "Thank you for your inquiry. We will get back to you shortly.",
