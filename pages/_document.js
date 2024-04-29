0f166b6cf245047149a2bc3f7a368d36179be1b9
de-salman <salmanfarisi.bca@gmail.com>
2024-04-29 10:13:04 +0400

    29-04 2

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,25 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import TagManager from "react-gtm-module";
 
 export default function Document() {
+  const tagManagerArgs = {
+    gtmId: "GTM-P5LRT5QZ",
+  };
+
+  TagManager.initialize(tagManagerArgs);
   return (
     <Html lang="en">
       <Head />
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P5LRT5QZ"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
