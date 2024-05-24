957cbb3dd3a46294f2ea89ca32c4f89dbb5174b2
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2024-05-24 13:13:18 +0530

    ssss

--- pages/_app.js
+++ pages/_app.js
@@ -117,6 +117,10 @@ export default function App({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0"
         />
+          <link rel="icon" type="image/png" sizes="192x192" href="/public/android-icon-192x192.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="96x96" href="/public/favicon-96x96.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png"/>
       </Head>
       <Header />
       <Component {...pageProps} />
