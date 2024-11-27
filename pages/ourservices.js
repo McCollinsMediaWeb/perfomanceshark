609a518c4b9b12543f5433b7fa40ca59408f9c30
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2024-11-27 16:31:21 +0530

    xxx

---
+++ pages/ourservices.js
@@ -0,0 +1,31 @@
+
+import Image from "next/image";
+import HeroBanner from "@/components/HeroBanner";
+import BestBrands from "@/components/BestBrands";
+import CaseStudies from "@/components/CaseStudies";
+import WhatWeDo from "@/components/WhatWedo";
+import VideoBox from "@/components/videobox";
+import OurPartners from "@/components/OurPartners";
+import OurServices from "@/components/OurServices";
+import WhyPErfomanceShark from "@/components/WhyPerfomanceShark";
+import ProofInNumber from "@/components/ProofInNumbers";
+import Testimonials from "@/components/Testimonials";
+import Announcement from "@/components/Announcement";
+import OurPackeges from "@/components/OurPackages";
+export default function Home() {
+  return (
+    <>
+  
+      <div id="six">
+        <OurPackeges/>
+      </div>
+      <div id="five">
+      <OurPartners/>
+      </div>
+     
+      <Announcement/>
+
+   
+    </>
+  );
+}
